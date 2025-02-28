d1918b34531b4aafd59bf70c1133a2a29ac2d0e0
seongjongju <sjj990480@email.com>
2025-02-28 22:07:51 +0900

    image관리

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import AlexaImage from './images/alexa.png';
+import SiriImage from './images/siri.png';
+
 function App() {
   return (
     <div>
       <p>hello image</p>
+      <img src={AlexaImage} alt='alexa' 
+      width={500} height={500} />
+      <img src={SiriImage} alt='siri' 
+      style={{width: 500, height: 500}}/>
     </div>
   );
 }
